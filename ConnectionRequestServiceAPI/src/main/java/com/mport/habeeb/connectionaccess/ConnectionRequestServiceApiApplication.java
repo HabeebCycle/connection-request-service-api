9c272373cff309244e09d0706b755739438cf9b9
Habeeb Okunade <guessy2k2@yahoo.com>
2019-01-30 14:49:35 +1100

    Update

---
+++ ConnectionRequestServiceAPI/src/main/java/com/mport/habeeb/connectionaccess/ConnectionRequestServiceApiApplication.java
@@ -0,0 +1,14 @@
+package com.mport.habeeb.connectionaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionRequestServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionRequestServiceApiApplication.class, args);
+	}
+
+}
+
